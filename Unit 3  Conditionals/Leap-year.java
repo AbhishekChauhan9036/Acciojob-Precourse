50fd841dcd563b259c1971b33f6f4d54a549adae
Abhi <abhishekchauhan9036@gmail.com>
2022-09-24 20:15:19 +0530

    Unit 3 half Done

---
+++ Unit 3  Conditionals/Leap-year.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Main {
+   public static void main (String[] args)
+   {
+
+       Scanner scanner = new Scanner(System.in);
+       int n = scanner.nextInt();
+       LeapYear(n);
+
+    
+}
+static void LeapYear(int n){
+     int p=0;
+    if(n%400==0){
+        p=1;
+    }else if(n%100==0){
+        p=0;
+    }else if(n%4==0){
+        p=1;
+    }else{
+        p=0;
+    }
+
+        System.out.print(p);
+  
+}
+}
